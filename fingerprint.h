3513c9de7b5ff83db1e2ef6541078734cfcfda3a
andrei-coman <andrei-coman@outlook.com>
2024-03-03 18:42:24 -0500

    Moved comparison logic to fingerprint.cpp; Stable on one machine

--- fingerprint.h
+++ fingerprint.h
@@ -8,6 +8,7 @@
 #include "utils.h"
 
 typedef std::array<std::array<long long, m>, n> fingerprint;
+constexpr double threshold = 0.5;
 
 /*
  * fingerprints 'fp_func' according to the m, n
@@ -17,6 +18,16 @@ typedef std::array<std::array<long long, m>, n> fingerprint;
  */
 fingerprint make_fingerprint(const std::function<size_t(size_t)>& fp_func, const std::string& out = "");
 
+/*
+ * reads and returns a fingerprint from 'in'.
+ */
 fingerprint read_fingerprint(const std::string& in);
+
+/*
+ * checks if fingerprints A and B match. This
+ * is implemented according to section 3.3.2
+ * and Figure 2 of the original paper.
+ */
+bool match(fingerprint A, fingerprint B);
 
 #endif
--- main.cpp
+++ main.cpp
@@ -6,45 +6,8 @@
 #include "fp_funcs.h"
 #include "utils.h"
 
-#include <map>
-
 // which function is used for fingerprinting
 constexpr Method method = HASH;
-
-typedef std::array<std::map<long long, int>, n> m_fingerprint;
-constexpr double threshold = 0.5;
-
-m_fingerprint to_map(fingerprint fp) {
-	m_fingerprint m_fp;
-	for(int i = 0; i < n; i++)
-		for(const long long& measurement: fp[i])
-			m_fp[i][measurement]++;
-	return m_fp;
-}
-
-long long mode(std::map<long long, int> m) {
-	auto x = std::max_element(
-					m.begin(), m.end(),
-    				[](const std::pair<long long, int>& p1, const std::pair<long long, int>& p2) {
-        				return p1.second < p2.second; 
-					});
-	return x->first;
-}
-
-bool match(fingerprint myfp, fingerprint base) {
-	m_fingerprint m_myfp = to_map(myfp);
-	m_fingerprint m_base = to_map(base);
-	
-	int count = 0;
-	for(int i = 0; i < n; i++) {
-		long long x = mode(m_myfp[i]);
-		count += m_base[i].find(x) != m_base[i].end();
-		long long y = mode(m_base[i]);
-		count += m_myfp[i].find(y) != m_base[i].end();
-	}
-	printf("%d %d\n", count, 2 * n);
-	return count >= threshold * 2 * n;
-}
 
 int main(int argc, char** argv) {
 	if (argc == 2 && strcmp(argv[1], "-cmp")) {
