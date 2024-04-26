2d55bfa7d005fbec5da34379dade049cc2d8c8ab
Yunzhou Li <yunzhouli0428@outlook.com>
2024-04-26 09:45:44 -0400

    shared array update

--- cryptofp-cpp/utils.cpp
+++ cryptofp-cpp/utils.cpp
@@ -15,3 +15,16 @@ void set_cpu(int cpu_id)
         perror("sched_setaffinity");
     }
 }
+
+void set_thread_cpu(pthread_t thread, int cpu_id)
+{
+    cpu_set_t mask;
+    int status;
+
+    CPU_ZERO(&mask);
+    CPU_SET(cpu_id, &mask);
+    status = pthread_setaffinity_np(thread, sizeof(mask), &mask);
+    if (status != 0) {
+        perror("pthread_setaffinity_np");
+    }
+}
--- cryptofp-cpp/utils.h
+++ cryptofp-cpp/utils.h
@@ -2,11 +2,13 @@
 #define __UTILS_H__
 
 #include <array>
+#include <pthread.h>
 
 // page 5, section 3.3.3
 constexpr int n = 1000;
 constexpr int m = 50;
 
 void set_cpu(int cpu_id);
+void set_thread_cpu(pthread_t thread,int cpu_id);
 
 #endif
